a653ba91a5d1d2372a77c5dfb53e27a3b7b41688
Marcelo Buzzeti <marcelobuzzetti@gmail.com>
2022-08-20 16:12:54 -0300

    login page

--- public/index.php
+++ public/index.php
@@ -1,15 +1,4 @@
 <?php
 
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-require_once(dirname(__FILE__, 2) . '/src/models/User.php');
-
-$user = new User(['name' => 'Lucas', 'email' => 'lucarcod3r@eb.br']);
-//echo $user->getSelect();
-
-print_r(User::get(['name' => 'Chaves'], 'id, name ,email'));
-
-echo '<br>';
-
-foreach(User::get([], 'name') as $user) {
-    echo $user->name, '<br>';
-}
+require_once(dirname(__FILE__, 2) . '/src/views/login.php');
