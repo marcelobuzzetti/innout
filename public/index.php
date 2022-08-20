9dd4f06e0e2e2432b2bed3317d0d874d8a8ece69
Marcelo Buzzeti <marcelobuzzetti@gmail.com>
2022-08-20 15:43:20 -0300

    improved get

--- public/index.php
+++ public/index.php
@@ -6,6 +6,10 @@ require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 $user = new User(['name' => 'Lucas', 'email' => 'lucarcod3r@eb.br']);
 //echo $user->getSelect();
 
-echo User::getSelect(['id' => 1], 'name ,email');
+print_r(User::get(['name' => 'Chaves'], 'id, name ,email'));
+
 echo '<br>';
-echo User::getSelect(['name' => 'Chaves', 'email' => 'chaves@cod3r.com.br']);
+
+foreach(User::get([], 'name') as $user) {
+    echo $user->name, '<br>';
+}
