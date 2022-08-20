df42ee2bd6ffa55d355226edb5e43a65dc4422e4
Marcelo Buzzeti <marcelobuzzetti@gmail.com>
2022-08-20 15:27:44 -0300

    query select user

--- public/index.php
+++ public/index.php
@@ -4,7 +4,8 @@ require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
 $user = new User(['name' => 'Lucas', 'email' => 'lucarcod3r@eb.br']);
-print_r($user);
-echo '<hr>';
-$user->email = 'marcelo@cod3r.com.br';
-print_r($user->email);
+//echo $user->getSelect();
+
+echo User::getSelect(['id' => 1], 'name ,email');
+echo '<br>';
+echo User::getSelect(['name' => 'Chaves', 'email' => 'chaves@cod3r.com.br']);
