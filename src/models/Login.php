bffe6f7473ba51c2feac3783d66a4ac7c01b31af
Marcelo Buzzetti <marcelobuzzetti@gmail.com>
2022-08-23 15:21:59 -0300

    Exception

--- src/models/Login.php
+++ src/models/Login.php
@@ -6,10 +6,13 @@ class Login extends Model {
     public function checkLogin() {
         $user = User::getOne(['email' => $this->email]);
         if($user) {
+            if($user->end_date) {
+                throw new AppException('Usuário está desligado da empresa.');
+            }
             if(password_verify($this->password, $user->password)) {
                 return $user;
             }
         }
-        throw new Exception();
+        throw new AppException('Usuário e/ou Senha inválidos.');
     }
 }
