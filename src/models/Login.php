178802b705c6dbc447307068d1b47d21fc147c21
Marcelo Buzzetti <marcelobuzzetti@gmail.com>
2022-08-31 15:07:59 -0300

    fields validation

--- src/models/Login.php
+++ src/models/Login.php
@@ -2,8 +2,26 @@
 loadModel('User');
 
 class Login extends Model {
+
+    public function validate()
+    {
+        $errors = [];
+
+        if(!$this->email) {
+            $errors['email'] = 'E-mail é um campo obrigatório';
+        }
+
+        if(!$this->password) {
+            $errors['password'] = 'Senha é um campo obrigatório';
+        }
+
+        if(count($errors) > 0) {
+            throw new ValidationException($errors);
+        }
+    }
     
     public function checkLogin() {
+        $this->validate();
         $user = User::getOne(['email' => $this->email]);
         if($user) {
             if($user->end_date) {
