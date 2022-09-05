89ecfeb4c8f2826aa447ff879c853842061b74de
Marcelo Buzzeti <marcelobuzzetti@gmail.com>
2022-09-05 16:08:14 -0300

    current date

--- src/controllers/day_records.php
+++ src/controllers/day_records.php
@@ -1,4 +1,12 @@
 <?php
 session_start();
 requireValidSession();
-loadTemplateView('day_records');
+
+/* Deprecated */
+/* $date = (new DateTime())->getTimestamp();
+$today = strftime('%d de %B de %Y', $date); */
+
+$formatter = new IntlDateFormatter('pt_BR', IntlDateFormatter::LONG, IntlDateFormatter::NONE);
+$today = $formatter->format(time());
+
+loadTemplateView('day_records', ['today' => $today]);
