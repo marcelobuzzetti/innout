c3c408c8d25914342c88ed8f971c19d14c35c8a5
Marcelo Buzzetti <marcelobuzzetti@gmail.com>
2022-09-08 14:45:55 -0300

    showing day records

--- src/controllers/data_generator.php
+++ src/controllers/data_generator.php
@@ -43,10 +43,11 @@ function getDayTemplateByOdds($regularRate, $extraRate, $lazyRate) {
 
 function populateWorkingHours($userId, $initialDate, $regularRate, $extraRate, $lazyRate){
     $currentDate = $initialDate;
-    $today = new DateTime();
+    $yesterday = new DateTime();
+    $yesterday->modify('-1 day');
     $columns = ['user_id' => $userId, 'work_date' => $currentDate];
 
-    while(isBefore($currentDate, $today)) {
+    while(isBefore($currentDate, $yesterday)) {
         if(!isWeekend($currentDate)) {
             $template = getDayTemplateByOdds($regularRate, $extraRate, $lazyRate);
             $columns = array_merge($columns, $template);
@@ -63,4 +64,4 @@ populateWorkingHours(1, date('Y-m-1'), 70, 20, 10);
 populateWorkingHours(3, date('Y-m-d', $lastMonth), 20, 75, 5);
 populateWorkingHours(4, date('Y-m-d', $lastMonth), 20, 10, 70);
 
-echo 'Tudo Certo';
+echo 'Tudo Certo!!!';
--- src/controllers/day_records.php
+++ src/controllers/day_records.php
@@ -2,6 +2,8 @@
 session_start();
 requireValidSession();
 
+loadModel('WorkingHours');
+
 /* Deprecated */
 /* $date = (new DateTime())->getTimestamp();
 $today = strftime('%d de %B de %Y', $date); */
@@ -9,4 +11,10 @@ $today = strftime('%d de %B de %Y', $date); */
 $formatter = new IntlDateFormatter('pt_BR', IntlDateFormatter::LONG, IntlDateFormatter::NONE);
 $today = $formatter->format(time());
 
-loadTemplateView('day_records', ['today' => $today]);
+$user = $_SESSION['user'];
+$records = WorkingHours::loadFromUserAndDate($user->id, date('Y-m-d'));
+
+loadTemplateView('day_records', [
+    'today' => $today,
+    'records' => $records
+]);
