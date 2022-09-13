b9bf18444bb316d66653951dbe5bef272541f001
Marcelo Buzzetti <marcelobuzzetti@gmail.com>
2022-09-13 15:08:38 -0300

    lunch interval

--- src/controllers/test.php
+++ src/controllers/test.php
@@ -5,5 +5,10 @@ loadModel('WorkingHours');
 
 $wh = WorkingHours::loadFromUserAndDate(1, date('Y-m-d'));
 
-$workedInterval = $wh->getWorkedInterval()->format('%H:%I:%S');
-print_r($workedInterval);
+$workedIntervalString = $wh->getWorkedInterval()->format('%H:%I:%S');
+print_r($workedIntervalString);
+echo '<hr>';
+
+$lunchIntervalString = $wh->getLunchInterval()->format('%H:%I:%S');
+print_r($lunchIntervalString);
+echo '<hr>';
