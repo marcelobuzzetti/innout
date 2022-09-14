fe87e0a13fc521cc31348f053ef8b7cd499d9a08
Marcelo Buzzetti <marcelobuzzetti@gmail.com>
2022-09-14 10:12:56 -0300

    show worked time

--- src/controllers/day_records.php
+++ src/controllers/day_records.php
@@ -11,10 +11,6 @@ $today = strftime('%d de %B de %Y', $date); */
 $formatter = new IntlDateFormatter('pt_BR', IntlDateFormatter::LONG, IntlDateFormatter::NONE);
 $today = $formatter->format(time());
 
-$user = $_SESSION['user'];
-$records = WorkingHours::loadFromUserAndDate($user->id, date('Y-m-d'));
-
 loadTemplateView('day_records', [
     'today' => $today,
-    'records' => $records
 ]);
