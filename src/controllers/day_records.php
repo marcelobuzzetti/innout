b4ede29c9a0f207d98508b608200b796c70ff957
Marcelo Buzzetti <marcelobuzzetti@gmail.com>
2022-09-15 11:06:50 -0300

    monthly report

--- src/controllers/day_records.php
+++ src/controllers/day_records.php
@@ -2,8 +2,6 @@
 session_start();
 requireValidSession();
 
-loadModel('WorkingHours');
-
 /* Deprecated */
 /* $date = (new DateTime())->getTimestamp();
 $today = strftime('%d de %B de %Y', $date); */
--- src/controllers/test.php
+++ src/controllers/test.php
@@ -1,16 +1,2 @@
 <?php
 //Controller temporario
-
-loadModel('WorkingHours');
-
-$wh = WorkingHours::loadFromUserAndDate(1, date('Y-m-d'));
-
-$workedIntervalString = $wh->getWorkedInterval()->format('%H:%I:%S');
-print_r($workedIntervalString);
-echo '<hr>';
-
-$lunchIntervalString = $wh->getLunchInterval()->format('%H:%I:%S');
-print_r($lunchIntervalString);
-echo '<hr>';
-
-print_r($wh->getExitTime());
