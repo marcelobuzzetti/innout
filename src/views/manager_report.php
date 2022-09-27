e863fc0165f9429f4e8dfe01f38d90be40462aa0
Marcelo Buzzeti <marcelobuzzetti@gmail.com>
2022-09-27 17:27:14 -0300

    manage report view

--- src/views/manager_report.php
+++ src/views/manager_report.php
@@ -1,9 +1,48 @@
 <main class="content">
-    <?php 
-        echo "Qtde de Usuário Ativos: {$activeUsersCount}";
-        echo '<br>';
-        echo "Horas trabalhadas no mês: {$hoursInMonth}";
-        echo '<br>';
-        print_r($absentUsers);
+    <?php
+        renderTitle(
+            'Relatório Gerencial',
+            'Resumo das Horas Trabalhadas dos funcionários',
+            'icofont-chart-histogram'
+        )
     ?>
+    <div class="summary-boxes">
+        <div class="summary-box bg-primary">
+            <i class="icofont-users"></i>
+            <p class="title">Qtde de Funcionários</p>
+            <h3 class="value"><?= $activeUsersCount ?></h3>
+        </div>
+        <div class="summary-box bg-danger">
+            <i class="icofont-patient-bed"></i>
+            <p class="title">Faltas</p>
+            <h3 class="value"><?= count($absentUsers) ?></h3>
+        </div>
+        <div class="summary-box bg-success">
+            <i class="icofont-sand-clock"></i>
+            <p class="title">Horas no mês</p>
+            <h3 class="value"><?= $hoursInMonth ?></h3>
+        </div>
+    </div>
+    <?php if(count($absentUsers) > 0): ?>
+        <div class="card mt-4">
+            <div class="card-header">
+                <h4 class="card-title">Faltosos do dia</h4>
+                <p class="card-category">Relação dos funcionários que ainda não bateram o ponto</p>
+            </div>
+            <div class="card-body">
+                <table class="table table-bordered table-striped table-hover">
+                    <thead>
+                        <th>Nome</th>
+                    </thead>
+                    <tbody>
+                        <?php foreach($absentUsers as $name): ?>
+                        <tr>
+                            <td><?= $name ?></td>
+                        </tr>
+                        <?php endforeach ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    <?php endif ?>
 </main>
