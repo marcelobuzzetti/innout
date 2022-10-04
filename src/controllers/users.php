8e60530df97d9176d66a43b0b10c4b66dff34480
Marcelo Buzzetti <marcelobuzzetti@gmail.com>
2022-10-04 10:55:15 -0300

    delete user

--- src/controllers/users.php
+++ src/controllers/users.php
@@ -2,6 +2,20 @@
 session_start();
 requireValidSession();
 
+$exception = null;
+if(isset($_GET['delete'])) {
+    try {
+        User::deleteById($_GET['delete']);
+        addSuccessMsg('Usuário excluído com sucesso!!!');
+    } catch (Exception $e) {
+        if(stripos($e->getMessage(), 'FOREIGN KEY')) {
+            addErrorMsg('Não é possível excluir o usuário com registros de ponto!!!');
+        } else {
+            $exception = $e;
+        }
+    }
+}
+
 $users = User::get();
 foreach($users as $user) {
     $user->start_date = (new DateTime($user->start_date))->format('d/m/Y');
@@ -9,5 +23,6 @@ foreach($users as $user) {
 }
 
 loadTemplateView('users', [
-    'users' => $users
+    'users' => $users,
+    'exception' => $exception
 ]);
